ebd1c45092406b0db2a4d2a0572eccce14fdd0be
Erwan SINCK <erwan.sinck12@gmail.com>
2025-11-25 19:13:29 +0100

    clean auth

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,37 +1,5 @@
 const AUTH_URL = "http://localhost:3000";
 
-export const register = async (username: string, password: string) => {
-	const res = await fetch(`${AUTH_URL}/auth/login`, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ username, password }),
-	});
-
-	if (!res.ok) {
-		const err = await res.json();
-		throw new Error(err.error || "Erreur lors de l'inscription");
-	}
-
-	return res.json();
-};
-
-export const login = async (username: string, password: string) => {
-	const res = await fetch(`${AUTH_URL}/auth/login`, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ username, password }),
-	});
-
-	if (!res.ok) {
-		const err = await res.json();
-		throw new Error(err.error || "Identifiants invalides");
-	}
-
-	const data = await res.json();
-	localStorage.setItem("token", data.token);
-	return data;
-};
-
 export const getCurrentUser = async () => {
 	const token = localStorage.getItem("token");
 	if (!token) throw new Error("Pas de token");
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,12 @@
-import { login } from "@/auth";
 import DiscordButton from "@/componants/discordButton";
 import GoogleButton from "@/componants/googleButton";
 import { useState } from "react";
 
 export default function Login() {
-	// const [username, setUsername] = useState("");
-	// const [password, setPassword] = useState("");
-	// const [error, setError] = useState("");
-
-	// async function handleLogin(e: React.FormEvent) {
-	//     e.preventDefault();
-	//     try {
-	//         await login(username, password);
-	//         alert('Connexion réussie !');
-	//         window.location.href = "/profile";
-	//     } catch (err: any) {
-	//         setError(err.message);
-	//     }
-	// }
 
 	return (
 		<div>
-			<h2>Connexion</h2>
-			{/* <form onSubmit={handleLogin}>
-                <h2>Connexion</h2>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input 
-                    type="password"
-                    placeholder="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">login</button>
-                {error && <p style={{color:"red"}}>{error}</p>}
-            </form> */}
+			<h2>Connexion : </h2>
 			<br />
 			<GoogleButton/>
 			<br />
